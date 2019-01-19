ffdf288137a632e7ec7522a315269d65471b7250
Николай <a6ashin@gmail.com>
2019-01-20 00:52:02 +0300

    2 commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   resources :debts
   resources :users
   get '/signin', to: 'users#signin'
+  get '/all', to: 'debts#all'
+  get '/all_users', to: 'users#all_users'
+  get '/new_user', to: 'users#new_user'
+  get '/index', to: 'users#index'
   post '/login', to: 'users#login'
 
 
-  root "users#index"
+  root "users#main"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
