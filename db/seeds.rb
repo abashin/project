e4e9ae18f45064bb7579a10131740f36be2011eb
Николай <a6ashin@gmail.com>
2019-01-25 14:23:48 +0300

    5 commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'a6ashin@gmail.com', password: 'nick2004', name: 'Николай', lastname: "Абашин", patronymic: 'Александрович', passport_series: '6012', passport_number: '188258', address: "г. Москва, ул Иванова, д 6, стр 2, кв 45", passport_institution: 'увд Москвы мещанского района', passport_date: '18.04.18', right_id: '1')
+User.create(email: 'ivan991@gmail.com', password: 'ivan1996', name: 'Иван', lastname: "Семёнов", patronymic: 'Кириллович', passport_series: '3204', passport_number: '174561', address: "г. Москва, ул Новослободская, д 16, стр 1, кв 14", passport_institution: 'увд Москвы цао', passport_date: '01.01.1986', right_id:1)
+#существует 2 вида пользователей - админ и обычный, админ может просматривать все созданые расписки и пользователей, обычный юзверь может просматривать только созданные им расписки, также я реализовал автозаполнение при создании новой расписки
